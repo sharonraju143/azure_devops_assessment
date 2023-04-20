08d62ba3a7da9099de0a9f6939a21af4d1cdad08
sharonraju143 <sharon.60494@gmail.com>
2023-04-20 17:48:56 +0530

    files added

---
+++ Infra_files/vars.tf
@@ -0,0 +1,16 @@
+variable "location" {
+    type = string
+    default = "East US"
+  
+}
+
+variable "resource_group_name" {
+    type = string
+    default = "hms-demo"
+  
+}
+variable "environment" {
+    type = string
+    default = "Dev"
+
+}
